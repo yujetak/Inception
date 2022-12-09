4642d0b871ca0312c82cd74f09613ba6dfe7256f
42yotak <yotak@student.42.seoul>
2022-12-09 21:08:34 +0900

    add

--- srcs/requirements/wordpress/tools/entry.sh
+++ srcs/requirements/wordpress/tools/entry.sh
@@ -1,1 +1,6 @@
-while true; do { sleep 1;}; done;
+# wordpress download
+wget https://wordpress.org/latest.tar.gz && tar -xzvf latest.tar.gz
+mv /wordpress/* /var/www/html
+rm -rf /wordpress latest.tar.gz
+
+exec php-fpm7 -F
