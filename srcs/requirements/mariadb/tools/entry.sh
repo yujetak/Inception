5aa520fbc7373192f59541668a11c9a389a54c1e
42yotak <yotak@student.42.seoul>
2022-12-09 17:19:28 +0900

    wordpress confs

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -10,13 +10,13 @@ mysql_install_db --user=root \
 
 rc-update add mariadb && sleep 10
 
-mysql -e "CREATE DATABASE IF NOT EXISTS '${MYSQL_DB}'";
+mysql -e "CREATE DATABASE IF NOT EXISTS '${DB}'";
 
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_DB_USER}'@'%' IDENTIFIED BY '${MYSQL_DB_USER_PASSWORD}'";
+mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}'";
 
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_DB_USER}'@'%'";
+mysql -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${DB_USER}'@'%'";
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'";
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}'";
 
 myqle -e "FLUSH PRIVILEGES;"
 
