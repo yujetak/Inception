debdd561b949282866b9743b35eeb43c7ac1e72d
42yotak <yotak@student.42seoul.kr>
2022-12-20 10:39:04 +0900

    env mode

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv("DB") );
+define( 'DB_NAME', getenv("DB_NAME") );
 
 /** Database username */
-define( 'DB_USER', getenv("DB_ROOT") );
+define( 'DB_USER', getenv("USER") );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv("DB_ROOT_PASSWORD") );
+define( 'DB_PASSWORD', getenv("USER_PASSWORD") );
 
 /** Database hostname */
 define( 'DB_HOST', 'mariadb' );
